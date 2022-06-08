22df0ca3c3fc82ecf13f5aa66dfe5aed569ed52e
mickiy123 <mikikinfe345@gmail.com>
2022-06-08 08:26:11 +0300

    Fix: use betty documentation for coding

--- Recursion/sum_of_natural_number.c
+++ Recursion/sum_of_natural_number.c
@@ -10,22 +10,20 @@
 
 int sum(int n)
 {
-        return (n*(n + 1) / 2);
+	return (n * (n + 1) / 2);
 }
 
 /**
  * main - Entry point
  *
  *
- * Description: print sum of first n natural numbers
- *              example: if n == 5
- *                       output = 15
  * Return: Always 0 success
  */
 
 int main(void)
 {
 	int result = sum(10);
+
 	printf("%d", result);
 	return (0);
 }
