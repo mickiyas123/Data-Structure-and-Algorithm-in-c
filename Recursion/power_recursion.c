316b4a2a0864034023186d938c47f28b5b3816f9
mickiy123 <mikikinfe345@gmail.com>
2022-06-08 15:32:44 +0300

    Add: calculate power of a number using recursion

---
+++ Recursion/power_recursion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * power - A recurtion function that calculates the power of a given number
+ * @m: base number
+ * @n: exponent
+ *
+ * Return: base number raised to exponent
+ */
+
+int power(int m, int n)
+{
+	if (n == 0)
+		return (1);
+
+	if (n % 2 == 0)
+	{
+		return (power(m * m, n / 2));
+	}
+	else
+	{
+		return (m * (m * m, (n - 1 / 2)));
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 succesful
+ */
+int main(void)
+{
+	int k;
+
+	k = power(2, 4);
+	printf("%d\n", k);
+	return (0);
+}
