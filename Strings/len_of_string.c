aba18bef0bc5e667b0e3ac3576ae1f41ec53d484
mickiy123 <mikikinfe345@gmail.com>
2022-06-09 17:26:45 +0300

    Add: impliment strlen

---
+++ Strings/len_of_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Decription: This function shows how to get the length of a string
+ *             Note: strlen function from string.h library can be used
+ *                   to impliment the function below
+ * Return: Always 0 successful
+ */
+
+int main(void)
+{
+	char *s = "Something nice";
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	printf("Lenght of s is %d\n", i);
+	return (0);
+}
