b968f2f4295b6676d1ad285689ef690ded1a6da7
mickiyas123 <mikikinfe345@gmail.com>
2022-12-25 14:18:46 +0300

    Complete binary search using recursion

---
+++ ArrayAdt/array_binary_search_recursive.c
@@ -0,0 +1,51 @@
+#include "array.h"
+
+/**
+ * binarySearch - search for a given value in array
+ *
+ * @arr: array to be searched
+ * @value: value to be searched in given array
+ * @low: the first array value index
+ * @high: the lat array value index
+ *
+ * Return: index of the value if found -1 if not
+ *
+*/
+
+int binarySearch(int arr[], int value, int low, int high)
+{
+	int middle;
+
+	if (low <= high)
+	{
+		middle = (low + high) / 2;
+		if (value == arr[middle])
+		{
+			return (middle);
+		}
+		else if (value < arr[middle])
+		{
+			return (binarySearch(arr, value, low, middle - 1));
+		}
+		else if (value > arr[middle])
+		{
+			return (binarySearch(arr, value, middle + 1, high));
+		}
+	}
+	return (-1);
+}
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 Success
+*/
+
+int main(void)
+{
+	struct Array arr = {{1, 2, 3, 4, 5}, 20, 5};
+	int i = 0;
+
+	printf("%d ", binarySearch(arr.A, 8, 0, arr.length));
+	return (0);
+}
